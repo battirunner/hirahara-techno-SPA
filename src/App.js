ab8483d165da16c5c15d067a66fcc86aa96ab8ea
battirunner <fahimislammohip@gmail.com>
2023-08-23 01:11:43 +0900

    first initializing commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Routes, Route, NavLink } from "react-router-dom";
+// We will create these two pages in a moment
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route path="/:id" element={<AboutPage />} />
+    </Routes>
   );
 }
 
